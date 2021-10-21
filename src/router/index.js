e1a759eedd12216b85dd703ebc9212465bc40a05
juan-skill <1294@holbertonschool.com>
2021-10-21 04:27:26 +0000

    Configure login and signup components

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
 import App from '../App.vue'
+import Login from '../components/Login.vue'
+import SignUp from '../components/SignUp.vue'
 
 const routes = [
   {
     path: '/',
     name: 'root',
     component: App
-  }
+  },
+  {
+    path: '/user/signUp',
+    name: "signUp",
+    component: SignUp
+  },
+  {
+    path: '/user/logIn',
+    name: "logIn",
+    component: Login
+  },  
 ]
 
 const router = createRouter({
